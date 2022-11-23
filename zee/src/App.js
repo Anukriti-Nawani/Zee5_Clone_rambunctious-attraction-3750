42f603d2fcc7ecc5ffb4b5d3119ca6af4b4b10b4
Anukriti-Nawani <anunwn@gmail.com>
2022-11-23 17:42:12 +0530

    files

---
+++ zee/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import AllRoutes from "./Routes/AllRoutes";
+import Navbar from "./Components/Navbar";
+import Footer from "./Components/Footer";
+
+function App() {
+  return (
+    <div className="App">
+      <AllRoutes />
+    </div>
+  );
+}
+
+export default App;
---
+++ zee/src/Routes/AllRoutes.jsx
@@ -0,0 +1,34 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./Home";
+import Login from "./Login";
+import Movies from "./Movies";
+import Signup from "./Signup";
+import TVShows from "./TVShows";
+import SignUpOtp from "./SignupOtp";
+import PrivateRoute from "../Components/PrivateRoute";
+import LoginOtp from "./LoginOtp";
+import BuyPLan from "./BuyPlan";
+
+function AllRoutes() {
+  return (
+    <Routes>
+      <Route
+        path="/"
+        element={
+        
+            <Home />
+         
+        }
+      />
+      <Route path="/login" element={<Login />} />
+      <Route path="/loginotp" element={<LoginOtp />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/signupotp" element={<SignUpOtp />} />
+      <Route path="/tvShows" element={<TVShows />} />
+      <Route path="/movies" element={<PrivateRoute><Movies /></PrivateRoute>} />
+      <Route path="/buyplan" element={<PrivateRoute><BuyPLan /></PrivateRoute>} />
+    </Routes>
+  );
+}
+
+export default AllRoutes;
